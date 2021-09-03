3385bc27d1de64e2ce13d2c0972787bd2f3ab71b
Santosh <thetechgig@gmail.com>
2021-09-03 21:35:38 +0530

    updated by GitA

--- src/test/java/qalclickacademy/BrowserTest.java
+++ src/test/java/qalclickacademy/BrowserTest.java
@@ -8,5 +8,6 @@ public class BrowserTest {
 		System.out.println("Test chrome browser");
 		System.out.println("Test Firefox browser");
 		System.out.println("Test safari browser");
+		System.out.println("Test all browsers");
 	}
 }

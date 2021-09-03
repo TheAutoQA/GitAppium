2892e772f152fa64d9fcafc3d20407a67631c78f
Santosh <thetechgig@gmail.com>
2021-09-03 22:48:36 +0530

    Committed by GitA User in develop

---
+++ src/test/java/qalclickacademy/AndriodMobile.java
@@ -0,0 +1,15 @@
+package qalclickacademy;
+
+public class AndriodMobile {
+
+	public void Redmi1()
+	{
+		System.out.println("Test for Redmi1");
+	}
+	
+	public void MI10()
+	{
+		System.out.println("This for Mi10");
+	}
+	
+}
---
+++ src/test/java/qalclickacademy/IOSMobile.java
@@ -0,0 +1,13 @@
+package qalclickacademy;
+
+public class IOSMobile {
+
+	public void Iphone10()
+	{
+		System.out.println("This test for iphone10");
+	}
+public void iphone12()
+{
+	System.out.println("This test is for iphone 12");
+}
+}
